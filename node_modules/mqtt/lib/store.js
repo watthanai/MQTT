b9e659b163e016ec79dc0f064471a3b213ac1d0e
Watthanai <watthanai2540@gmail.com>
2021-03-06 19:53:05 +0700

    MQTT

--- node_modules/mqtt/lib/store.js
+++ node_modules/mqtt/lib/store.js
@@ -1,18 +1,33 @@
-'use strict';
-var Readable = require('readable-stream').Readable,
-  streamsOpts = { objectMode: true };
+'use strict'
+
+/**
+ * Module dependencies
+ */
+var xtend = require('xtend')
+
+var Readable = require('readable-stream').Readable
+var streamsOpts = { objectMode: true }
+var defaultStoreOptions = {
+  clean: true
+}
 
 /**
  * In-memory implementation of the message store
  * This can actually be saved into files.
  *
+ * @param {Object} [options] - store options
  */
-function Store () {
+function Store (options) {
   if (!(this instanceof Store)) {
-    return new Store();
+    return new Store(options)
   }
 
-  this._inflights = {};
+  this.options = options || {}
+
+  // Defaults
+  this.options = xtend(defaultStoreOptions, options)
+
+  this._inflights = new Map()
 }
 
 /**
@@ -21,88 +36,93 @@ function Store () {
  *
  */
 Store.prototype.put = function (packet, cb) {
-  this._inflights[packet.messageId] = packet;
+  this._inflights.set(packet.messageId, packet)
 
   if (cb) {
-    cb();
+    cb()
   }
 
-  return this;
-};
+  return this
+}
 
 /**
  * Creates a stream with all the packets in the store
  *
  */
 Store.prototype.createStream = function () {
-  var stream = new Readable(streamsOpts),
-    inflights = this._inflights,
-    ids = Object.keys(this._inflights),
-    destroyed = false,
-    i = 0;
+  var stream = new Readable(streamsOpts)
+  var destroyed = false
+  var values = []
+  var i = 0
+
+  this._inflights.forEach(function (value, key) {
+    values.push(value)
+  })
 
   stream._read = function () {
-    if (!destroyed && i < ids.length) {
-      this.push(inflights[ids[i++]]);
+    if (!destroyed && i < values.length) {
+      this.push(values[i++])
     } else {
-      this.push(null);
+      this.push(null)
     }
-  };
+  }
 
   stream.destroy = function () {
     if (destroyed) {
-      return;
+      return
     }
 
-    var self = this;
+    var self = this
 
-    destroyed = true;
+    destroyed = true
 
-    process.nextTick(function () {
-      self.emit('close');
-    });
-  };
+    setTimeout(function () {
+      self.emit('close')
+    }, 0)
+  }
 
-  return stream;
-};
+  return stream
+}
 
 /**
  * deletes a packet from the store.
  */
 Store.prototype.del = function (packet, cb) {
-  packet = this._inflights[packet.messageId];
+  packet = this._inflights.get(packet.messageId)
   if (packet) {
-    delete this._inflights[packet.messageId];
-    cb(null, packet);
+    this._inflights.delete(packet.messageId)
+    cb(null, packet)
   } else if (cb) {
-    cb(new Error('missing packet'));
+    cb(new Error('missing packet'))
   }
 
-  return this;
-};
+  return this
+}
 
 /**
  * get a packet from the store.
  */
 Store.prototype.get = function (packet, cb) {
-  packet = this._inflights[packet.messageId];
+  packet = this._inflights.get(packet.messageId)
   if (packet) {
-    cb(null, packet);
+    cb(null, packet)
   } else if (cb) {
-    cb(new Error('missing packet'));
+    cb(new Error('missing packet'))
   }
 
-  return this;
-};
+  return this
+}
 
 /**
  * Close the store
  */
 Store.prototype.close = function (cb) {
-  this._inflights = null;
-  if (cb) {
-    cb();
+  if (this.options.clean) {
+    this._inflights = null
   }
-};
+  if (cb) {
+    cb()
+  }
+}
 
-module.exports = Store;
+module.exports = Store
