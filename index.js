2252223f4965f531c7ad1e672f05c28504afe193
Watthanai <watthanai2540@gmail.com>
2021-03-07 02:29:25 +0700

    updatr

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ var ascoltatore = {
 };
 
 var settings = {
-  port: 1883,
-  http:{
-      port:8883
-  }
+  port: process.env.PORT || 1883,
+//   http:{
+//       port:process.env.PORT_HTTP || 8883
+//   }
 };
 
 var server = new mosca.Server(settings);
 
 server.on('clientConnected', function(client) {
     console.log('client connected', client.id);
-    
+
 });
 
 // fired when a message is received
