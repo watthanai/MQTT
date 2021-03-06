57836b598f0e88a9e67adcda76b671f0d68818cf
Watthanai <watthanai2540@gmail.com>
2021-03-07 01:36:08 +0700

    updatr

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-var http = require('http'),
-    httpServ = http.createServer(),
-    mosca = require('mosca'),
-    mqttServ = new mosca.Server({});
+// var http = require('http'),
+//     httpServ = http.createServer(),
+//     mosca = require('mosca'),
+//     mqttServ = new mosca.Server({});
 
-mqttServ.attachHttpServer(httpServ); 
+// mqttServ.attachHttpServer(httpServ); 
 
-httpServ.listen(process.env.PORT || 8080,()=>console.log("Server is running"));
+// httpServ.listen(process.env.PORT || 8080,()=>console.log("Server is running"));
+
+var mosca = require('mosca');
+
+var ascoltatore = {
+  //using ascoltatore
+  type: 'mongo',
+  pubsubCollection: 'Broker',
+  mongo: {}
+};
+
+var settings = {
+  port: 1883,
+  http:{
+      port:8883
+  }
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
 
 
 
