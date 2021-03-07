d275610dcd43499e43d00a053159431869a84f97
Watthanai <watthanai2540@gmail.com>
2021-03-07 13:42:48 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // httpServ.listen(process.env.PORT || 1883,()=>console.log("Server is running"));
 
-var mosca = require('mosca');
+// var mosca = require('mosca');
 
 // var ascoltatore = {
 //   //using ascoltatore
@@ -16,30 +16,58 @@ var mosca = require('mosca');
 //   mongo: {}
 // };
 
-var settings = {
+// var settings = {
    
-//   port: process.env.PORT || 1883,
-     port: 1883
+// //   port: process.env.PORT || 1883,
+//      port: app.listen(process.env.PORT || 1883)
+// };
+
+// var server = new mosca.Server(settings);
+
+// server.on('clientConnected', function(client) {
+//     console.log('client connected', client.id);
+
+// });
+
+// // fired when a message is received
+// server.on('published', function(packet, client) {
+//   console.log('Published', packet.payload);
+// });
+
+// server.on('ready', setup);
+
+// // fired when the mqtt server is ready
+// function setup() {
+//   console.log('Mosca server is up and running');
+// }
+
+var express = require('express');
+var http = require('http');
+var mosca = require('mosca');
+
+var app = express();
+var server = http.createServer(app);
+
+var pubsubsettings = {
+    type: 'mongo',
+    url: process.env.MONGOLAB_URI ||  'mongodb://localhost:27017/app',
+    pubsubCollection: 'mqtt',
+    mongo: {}
 };
 
-var server = new mosca.Server(settings);
-
-server.on('clientConnected', function(client) {
-    console.log('client connected', client.id);
-
+var server = new mosca.Server({
+    backend: pubsubsettings,
+    persistence: {
+        factory: mosca.persistence.Mongo,
+        url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/app'
+    }
+}, function() {
+    server.attachHttpServer(app);
 });
 
-// fired when a message is received
-server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+server.on('ready', function() {
+    console.log('Mosca is running');
 });
-
-server.on('ready', setup);
-
-// fired when the mqtt server is ready
-function setup() {
-  console.log('Mosca server is up and running');
-}
 
 
 
